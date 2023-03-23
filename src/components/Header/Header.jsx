2688f8e6e938da5103e3a5195e1a2dce7d4fdeca
Iva Krstik <krstikiv@gmail.com>
2023-03-23 23:10:00 +0100

    setting up blog

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className='bg-white'>
+            <p>BLOG? It's Easy If You Do It Smart</p>
+            <h1>
+                <span>"</span>BLOGLIKE<span>"</span>
+            </h1>
+            <p>
+            Congratulations! <br /> Your BLOG Is About To Stop Being Relevant
+
+            </p>
+        </div>
+    )
+}
+
+export default Header;
